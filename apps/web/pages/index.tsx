491fa2a009e8da39b5226f40cbab55dfa1d323b9
Hourmeng Khy <itshourmeng@gmail.com>
2022-12-06 14:16:46 +0700

    add button.tsx

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -2,9 +2,9 @@ import { Button } from 'ui';
 
 export default function Web() {
   return (
-    <div>
+    <div className="my-20 flex flex-col items-center justify-center space-y-8">
       <h1 className="text-2xl text-gray-900">Web</h1>
-      <Button />
+      <Button varaint="secondary">Login</Button>
     </div>
   );
 }
--- packages/ui/src/Button.tsx
+++ packages/ui/src/Button.tsx
@@ -1,4 +1,24 @@
-import * as React from 'react';
-export const Button = () => {
-  return <button className="bg-gray-200 py-2 px-4">Boop</button>;
+import React from 'react';
+
+interface ButtonProps {
+  children: React.ReactNode | string;
+  varaint?: 'primary' | 'secondary';
+}
+
+const varaintClassname = {
+  primary: 'bg-primary text-white shadow-inner',
+  secondary: 'bg-white text-gray-900 border border-gray-200',
+};
+
+export const Button: React.FC<ButtonProps> = ({
+  varaint = 'primary',
+  children,
+}) => {
+  return (
+    <button
+      className={`rounded-2xl px-8 py-4 font-medium ${varaintClassname[varaint]}`}
+    >
+      {children}
+    </button>
+  );
 };
