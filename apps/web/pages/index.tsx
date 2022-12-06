ce083f4f95d7722651aa246022b6e5256030399d
Hourmeng Khy <itshourmeng@gmail.com>
2022-12-06 16:10:12 +0700

    add ButtonInterested.tsx

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,14 +1,15 @@
-import { Button, ButtonCategory, ButtonLink } from 'ui';
+import { Button, ButtonCategory, ButtonInterested, ButtonLink } from 'ui';
 
 export default function Web() {
   return (
-    <div className="my-20 flex flex-col items-center justify-center space-y-8">
+    <div className="my-20 mx-auto flex max-w-sm flex-col items-center justify-center space-y-8 px-4">
       <h1 className="text-2xl text-gray-900">Web</h1>
       <Button varaint="secondary">Login</Button>
       <ButtonCategory isActive>Recently</ButtonCategory>
       <ButtonLink href="https://www.facebook.com/" varaint="secondary">
         Link
       </ButtonLink>
+      <ButtonInterested hasText />
     </div>
   );
 }
