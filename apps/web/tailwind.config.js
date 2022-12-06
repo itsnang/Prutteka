6f448e45bf277d0c8b3de99d815e96e948456a9c
Hourmeng Khy <itshourmeng@gmail.com>
2022-12-06 14:13:44 +0700

    setup tailwindcss config file

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -6,7 +8,31 @@ module.exports = {
     '../../packages/modules/src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    colors: {
+      primary: {
+        DEFAULT: '#FF006E',
+        dark: '#B3004D',
+      },
+      secondary: '#3A86FF',
+      tertiary: {
+        DEFAULT: '#FFBE0B',
+        ligth: '#FFF9E7',
+      },
+      white: '#fff',
+      black: '#000',
+      gray: colors.zinc,
+    },
+    extend: {
+      boxShadow: {
+        inner: 'inset 0 -6px 0 0 rgb(161, 161, 171, 25%)',
+      },
+      screens: {
+        desktop: '906px',
+      },
+      borderRadius: {
+        large: '12px',
+      },
+    },
   },
   plugins: [],
 };
---
+++ packages/modules/src/home/HomePage.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from 'next';
+
+interface HomePageProps {}
+
+export const HomePage:NextPage<HomePageProps> = () => {
+  return <div>HomePage</div>;
+};
