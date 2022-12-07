f907998fd51cfda4f028474831495b6cfe528775
Chhayyy <huotchhayyy@gmail.com>
2022-12-07 10:50:20 +0700

    add Footer.tsx

---
+++ packages/ui/src/Footer.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Image, { StaticImageData } from 'next/Image';
+
+interface FooterProps {
+  img: string | StaticImageData;
+  text?: string;
+}
+
+export const Footer: React.FC<FooterProps> = ({ img, text }) => {
+  return (
+    <footer className="border border-gray-100 text-gray-600">
+      <div className="flex w-screen flex-row items-center justify-between px-[186px] py-2">
+        <div>&copy; 2022</div>
+        <div className="relative h-8 w-20">
+          <Image src={img} alt="logo" fill />
+        </div>
+      </div>
+    </footer>
+  );
+};
--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -2,3 +2,4 @@ import * as React from 'react';
 export * from './Button';
 export * from './ButtonCategory';
 export * from './ButtonLink';
+export * from './Footer';
