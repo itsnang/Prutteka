3c9df024e2d360e7c38e237c7921c7c2233a4058
Hourmeng Khy <itshourmeng@gmail.com>
2022-12-07 14:16:07 +0700

    add InputField.tsx & formik & yub

---
+++ packages/ui/src/InputField.tsx
@@ -0,0 +1,37 @@
+import React, { forwardRef } from 'react';
+
+import { useField } from 'formik';
+
+interface InputFieldProps extends Partial<HTMLInputElement> {
+  name: string;
+  type?: string;
+  label?: string;
+  className?: string;
+}
+
+export const InputField = forwardRef<HTMLInputElement, InputFieldProps>(
+  ({ name, type = 'text', label, className, ...props }, ref) => {
+    const [field, meta] = useField(name);
+
+    return (
+      <div className="space-y-2">
+        <label className="flex flex-col">
+          {label}
+          <input
+            className={`h-13 focus:ring-primary rounded-2xl border border-gray-200 px-4 text-gray-900 focus:outline-none focus:ring 
+            ${className} ${label ? 'mt-2' : ''}`}
+            ref={ref}
+            type={type}
+            {...field}
+            {...props}
+          />
+        </label>
+        {meta.error && meta.touched && (
+          <div className="text-red-600">{meta.error}</div>
+        )}
+      </div>
+    );
+  }
+);
+
+InputField.displayName = 'input';
