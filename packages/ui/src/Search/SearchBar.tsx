08b69b7ae5d4eca987635fb9adcc7caac225c905
Chhayyy <huotchhayyy@gmail.com>
2022-12-08 14:09:07 +0700

    add SearchBar.tsx

---
+++ packages/ui/src/Search/SearchBar.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { useState } from 'react';
+
+import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+
+interface SearchBarProps {
+  placeholder: string;
+  onSearch: (e: React.FormEvent<HTMLFormElement>, input: string) => void;
+}
+
+export const SearchBar: React.FC<SearchBarProps> = ({
+  placeholder,
+  onSearch,
+}) => {
+  const [input, setInput] = useState('');
+
+  return (
+    <form
+      className=" flex items-center rounded-2xl border px-4 py-[0.875rem]"
+      onSubmit={(e) => onSearch(e, input)}
+    >
+      <input
+        type="text"
+        placeholder={placeholder}
+        className=" border-gray-200 outline-none"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button type="submit">
+        <MagnifyingGlassIcon className="h-6 w-6 text-gray-600" />
+      </button>
+    </form>
+  );
+};
