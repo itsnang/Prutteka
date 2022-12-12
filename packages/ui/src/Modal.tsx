2e013d02243e48a2538bc567b077240571bd8935
Sean-Sophearom <sean.sophearom77@gmail.com>
2022-12-12 09:06:12 +0700

    Re-factor modal component

--- packages/ui/src/Modal.tsx
+++ packages/ui/src/Modal.tsx
@@ -1,10 +1,12 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { Fragment } from 'react';
 import { XMarkIcon } from '@heroicons/react/24/solid';
+import { Dialog, Transition } from '@headlessui/react';
 
 interface ModalProps {
   children: React.ReactNode | string;
   show: boolean;
-  title: string;
+  title?: string;
+  className?: string;
   onClose: () => void;
 }
 
@@ -13,45 +15,54 @@ export const Modal: React.FC<ModalProps> = ({
   onClose,
   children,
   title,
+  className = '',
 }) => {
-  const modalRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    if (document) {
-      if (show) document.body.classList.add('overflow-hidden');
-      else document.body.classList.remove('overflow-hidden');
-    }
-  }, [show]);
-
-  const handleClickOutside = (e: React.MouseEvent<HTMLDivElement>) => {
-    if (!modalRef?.current?.contains(e.target as Node)) onClose();
-  };
-
-  if (show)
-    return (
-      <div
-        style={{
-          margin: 0,
-          translate: 0,
-          transform: 'translate(0)',
-          padding: 0,
-          top: 0,
-          left: 0,
-        }}
-        className="fixed top-0 left-0 flex h-screen w-screen translate-x-2 items-center justify-center bg-black bg-opacity-25"
-        onClick={handleClickOutside}
-      >
-        <div className="space-y-6 rounded-2xl bg-white p-6" ref={modalRef}>
-          <div className="flex justify-between">
-            <h4>{title}</h4>
-            <button onClick={onClose}>
-              <XMarkIcon width={24} />
-            </button>
+  return (
+    <Transition appear show={show} as={Fragment}>
+      <Dialog as="div" onClose={onClose} className="relative z-10">
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-black bg-opacity-25" />
+        </Transition.Child>
+        <div className="fixed inset-0 overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-4">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <Dialog.Panel
+                className={
+                  'w-full max-w-md transform rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all ' +
+                  className
+                }
+              >
+                <Dialog.Title
+                  as="h3"
+                  className="items-between flex justify-between text-lg font-medium leading-6 text-gray-900"
+                >
+                  <span>{title}</span>
+                  <button onClick={onClose}>
+                    <XMarkIcon width={24} />
+                  </button>
+                </Dialog.Title>
+                <div>{children}</div>
+              </Dialog.Panel>
+            </Transition.Child>
           </div>
-
-          <div>{children}</div>
         </div>
-      </div>
-    );
-  return null;
+      </Dialog>
+    </Transition>
+  );
 };
