4091b75a7c5d7a4a87b6a24f7d2a73eaa9e45871
Hourmeng Khy <itshourmeng@gmail.com>
2022-12-12 10:25:02 +0700

    refactor button & buttonlink to single component

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,9 +1,3 @@
-import { Button, ButtonCategory, ButtonInterested, ButtonLink } from 'ui';
-
 export default function Web() {
-  return (
-    <div className="my-20 mx-auto flex flex-col items-center justify-center space-y-8 px-4">
-      Hello World
-    </div>
-  );
+  return <div>Hello World</div>;
 }
