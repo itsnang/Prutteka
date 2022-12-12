9804d3eb47b72897cac1cf913884430dc9510b0e
Hourmeng Khy <itshourmeng@gmail.com>
2022-12-12 15:53:53 +0700

    add DesktopLayout.tsx

--- packages/modules/src/layouts/MainLayout.tsx
+++ packages/modules/src/layouts/MainLayout.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 interface MainLayoutProps {
   top?: React.ReactNode;
+  bottom?: React.ReactNode;
   className?: string;
   children: React.ReactNode | string;
 }
 
 export const MainLayout: React.FC<MainLayoutProps> = ({
   top,
+  bottom,
   className,
   children,
 }) => {
@@ -15,12 +17,13 @@ export const MainLayout: React.FC<MainLayoutProps> = ({
     <>
       {top}
       <div
-        className={`mx-auto max-w-5xl px-4 ${top ? 'mt-24' : ''} ${
+        className={`mx-auto min-h-screen max-w-5xl px-4 ${top ? 'mt-24' : ''} ${
           className ? className : ''
         }`}
       >
         {children}
       </div>
+      {bottom}
     </>
   );
 };
--- packages/ui/src/Footer.tsx
+++ packages/ui/src/Footer.tsx
@@ -1,18 +1,13 @@
 import React from 'react';
-import Image, { StaticImageData } from 'next/Image';
+import Image from 'next/image';
 
-interface FooterProps {
-  img: string | StaticImageData;
-  text?: string;
-}
-
-export const Footer: React.FC<FooterProps> = ({ img, text }) => {
+export const Footer: React.FC = () => {
   return (
-    <footer className="border border-gray-100 text-gray-600">
-      <div className="flex w-screen flex-row items-center justify-between px-[186px] py-2">
+    <footer className="border-t border-gray-100 text-gray-600">
+      <div className="mx-auto flex w-screen max-w-5xl items-center justify-between py-4">
         <div>&copy; 2022</div>
         <div className="relative h-8 w-20">
-          <Image src={img} alt="logo" fill />
+          <Image src="/Logo.png" alt="logo" fill />
         </div>
       </div>
     </footer>
