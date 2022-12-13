17cec6a024baa9d6e9fdec14aa1fac9f754c4117
Hourmeng Khy <itshourmeng@gmail.com>
2022-12-13 15:58:14 +0700

    add link to logo & search-bar & interested-button

--- packages/modules/src/layouts/Header.tsx
+++ packages/modules/src/layouts/Header.tsx
@@ -1,19 +1,35 @@
 import React from 'react';
-import Image, { StaticImageData } from 'next/image';
-import { SearchBar, ButtonInterested, Button } from 'ui';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+import { SearchBar, Button } from 'ui';
 import { StarIcon } from '@heroicons/react/24/solid';
+
 export const Header: React.FC = () => {
+  const router = useRouter();
+
   return (
     <nav className=" fixed top-0 z-10 w-screen border-b border-gray-100 bg-white">
       <div className="mx-auto flex max-w-5xl justify-between py-2">
         <div className="flex gap-4">
-          <Image src="/Logo.png" alt="Logo" height={52} width={132.5} />
-          <SearchBar placeholder="Search Events" onSearch={() => {}} />
+          <Link href="/">
+            <Image src="/Logo.png" alt="Logo" height={52} width={132.5} />
+          </Link>
+          <SearchBar
+            placeholder="Search Events"
+            onSearch={(e) => {
+              e.preventDefault();
+              router.push('/search');
+            }}
+          />
         </div>
         <div className="flex space-x-4 divide-x divide-gray-300">
           <div className="flex gap-2 ">
             <Button variant="secondary">EN</Button>
             <Button
+              as="link"
+              href="/user/interested"
               variant="secondary"
               icon={StarIcon}
               className="text-tertiary"
