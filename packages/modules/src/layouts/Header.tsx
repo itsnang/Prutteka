53cd15b7442295f68289b288fd2d06b29355440f
Hourmeng Khy <itshourmeng@gmail.com>
2022-12-13 15:01:27 +0700

    fix Header.tsx zIndex

--- packages/modules/src/layouts/Header.tsx
+++ packages/modules/src/layouts/Header.tsx
@@ -4,7 +4,7 @@ import { SearchBar, ButtonInterested, Button } from 'ui';
 import { StarIcon } from '@heroicons/react/24/solid';
 export const Header: React.FC = () => {
   return (
-    <nav className=" fixed top-0 w-screen border-b border-gray-100 bg-white">
+    <nav className=" fixed top-0 z-10 w-screen border-b border-gray-100 bg-white">
       <div className="mx-auto flex max-w-5xl justify-between py-2">
         <div className="flex gap-4">
           <Image src="/Logo.png" alt="Logo" height={52} width={132.5} />
