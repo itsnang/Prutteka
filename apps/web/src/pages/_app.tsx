79cd585410967ab2b625cf868379106a183f8d13
Hourmeng Khy <itshourmeng@gmail.com>
2022-12-16 09:45:17 +0700

    add favicon

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app';
 import type { ReactElement, ReactNode } from 'react';
 import type { NextPage } from 'next';
+import Head from 'next/head';
 
 import '../styles/globals.css';
 
@@ -24,9 +25,27 @@ Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
 
+const Header = () => (
+  <Head>
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, user-scalable=no, user-scalable=0"
+    />
+    <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
+  </Head>
+);
+
 export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout =
     Component.getLayout ?? ((page) => <DesktopLayout>{page}</DesktopLayout>);
 
-  return getLayout(<Component {...pageProps} />);
+  return getLayout(
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
