04ac82ddd5aa56fc97db721a2ba293980a0c0f01
Hourmeng Khy <itshourmeng@gmail.com>
2022-12-19 14:59:39 +0700

    add type-checking for useTranslation

---
+++ packages/modules/src/shared-hooks/useTypeSafeTranslation.ts
@@ -0,0 +1,25 @@
+import { useTranslation } from 'next-i18next';
+
+import translations from '../../../../apps/web/public/locales/en/translation.json';
+
+type Join<S1, S2> = S1 extends string
+  ? S2 extends string
+    ? `${S1}.${S2}`
+    : never
+  : never;
+
+type Paths<T> = {
+  [K in keyof T]: T[K] extends Record<string, unknown>
+    ? Join<K, Paths<T[K]>>
+    : K;
+}[keyof T];
+
+type TranslationKeys = Paths<typeof translations>;
+
+export const useTypeSafeTranslation = () => {
+  const { t } = useTranslation('translation');
+
+  return {
+    t: (s: TranslationKeys) => t(s),
+  };
+};
