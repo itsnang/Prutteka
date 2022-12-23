f4180a743f9d141002db79eddcd26101b3d46730
Hourmeng Khy <itshourmeng@gmail.com>
2022-12-23 10:41:35 +0700

    add responsive design to event nested page & search query

--- packages/modules/src/shared-hooks/index.ts
+++ packages/modules/src/shared-hooks/index.ts
@@ -1,2 +1,3 @@
 export * from './useTypeSafeTranslation';
 export * from './useLocalStorage';
+export * from './useDebounce';
--- packages/ui/src/Search/SearchBar.tsx
+++ packages/ui/src/Search/SearchBar.tsx
@@ -7,14 +7,18 @@ interface SearchBarProps {
   placeholder?: string;
   className?: string;
   onSearch: (e: React.FormEvent<HTMLFormElement>, input: string) => void;
+  value?: string;
+  onChange?: (value: React.SetStateAction<string>) => void;
 }
 
 export const SearchBar: React.FC<SearchBarProps> = ({
   placeholder,
   className = '',
   onSearch,
+  value = '',
+  onChange,
 }) => {
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState(value);
 
   return (
     <form
@@ -26,7 +30,11 @@ export const SearchBar: React.FC<SearchBarProps> = ({
         placeholder={placeholder}
         className="w-full border-gray-200 outline-none"
         value={input}
-        onChange={(e) => setInput(e.target.value)}
+        onChange={(e) => {
+          const value = e.target.value;
+          setInput(e.target.value);
+          onChange && onChange(value);
+        }}
       />
       <button type="submit">
         <MagnifyingGlassIcon className="h-5 w-5 text-gray-600 sm:h-6 sm:w-6" />
