6df7798aa426bcf694a4a15e75d6185589c76591
Hourmeng Khy <itshourmeng@gmail.com>
2022-12-27 10:13:41 +0700

    add localePath to next-i18next.config - fix translation on production

--- apps/web/next-i18next.config.js
+++ apps/web/next-i18next.config.js
@@ -1,3 +1,6 @@
+const path = require('path');
+
+const localePath = path.resolve('./public/locales');
 /**
  * @type {import('next').NextConfig}
  **/
@@ -6,4 +9,5 @@ module.exports = {
     locales: ['en', 'kh'],
     defaultLocale: 'en',
   },
+  localePath,
 };
