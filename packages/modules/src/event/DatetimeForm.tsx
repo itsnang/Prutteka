bc7826200169517f2697e65c56c1bba4eef1bc5b
Sean-Sophearom <sean.sophearom77@gmail.com>
2022-12-28 15:01:30 +0700

    update submitEventPage add khmer translation

--- packages/modules/src/event/DatetimeForm.tsx
+++ packages/modules/src/event/DatetimeForm.tsx
@@ -3,17 +3,21 @@ import { Field, FieldArray, useFormikContext } from 'formik';
 import { useEffect } from 'react';
 import { EventDetail } from '../type/EventDetailType';
 import { getCurrentTime } from './helper';
+import { t } from './submitEventPage';
+import { translateDate } from '../helpers';
 
 interface DatetimeFormProps {
   dateTimeState: EventDetail['datetime'];
   isInvalidInput: boolean;
   eventDays: Date[];
+  lang: 'en' | 'kh';
 }
 
 export const DatetimeForm: React.FC<DatetimeFormProps> = ({
   dateTimeState,
   isInvalidInput,
   eventDays,
+  lang,
 }) => {
   const { hasCustomTime, customTimes, startDate, endDate } = dateTimeState;
   const { setFieldValue } = useFormikContext();
@@ -30,7 +34,7 @@ export const DatetimeForm: React.FC<DatetimeFormProps> = ({
       ...dateTimeState,
       customTimes: newCustomTimes,
     });
-  }, [isInvalidInput, startDate, endDate]);
+  }, [isInvalidInput, eventDays]);
 
   const timeForm = hasCustomTime ? (
     <FieldArray name="datetime.customTimes">
@@ -39,21 +43,21 @@ export const DatetimeForm: React.FC<DatetimeFormProps> = ({
           {customTimes?.map(({ date }, idx) => (
             <div key={idx}>
               <Typography size="xl" weight="bold">
-                {date?.toDateString()}
+                {translateDate(date as Date, lang)}
               </Typography>
               <div className="flex gap-4">
                 <InputField
                   name={`datetime.customTimes.${idx}.startTime`}
-                  label="Start Time"
-                  placeholder="Start Time"
+                  label={t.startTime[lang]}
+                  placeholder={t.startTime[lang]}
                   containerClassName="flex-1"
                   type="time"
                 />
 
                 <InputField
                   name={`datetime.customTimes.${idx}.endTime`}
-                  label="End Time"
-                  placeholder="End Time"
+                  label={t.endTime[lang]}
+                  placeholder={t.endTime[lang]}
                   containerClassName="flex-1"
                   type="time"
                 />
@@ -67,16 +71,16 @@ export const DatetimeForm: React.FC<DatetimeFormProps> = ({
     <div className="flex gap-4">
       <InputField
         name="datetime.startTime"
-        label="Start Time"
-        placeholder="Start Time"
+        label={t.startTime[lang]}
+        placeholder={t.startTime[lang]}
         containerClassName="flex-1"
         className="w-full"
         type="time"
       />
       <InputField
         name="datetime.endTime"
-        label="End Time"
-        placeholder="End Time"
+        label={t.endTime[lang]}
+        placeholder={t.endTime[lang]}
         containerClassName="flex-1"
         className="w-full"
         type="time"
@@ -89,16 +93,16 @@ export const DatetimeForm: React.FC<DatetimeFormProps> = ({
       <div className="flex gap-4">
         <InputField
           name="datetime.startDate"
-          label="Start Date"
-          placeholder="Start Date"
+          label={t.startDate[lang]}
+          placeholder={t.startDate[lang]}
           containerClassName="flex-1"
           className="w-full"
           type="date"
         />
         <InputField
           name="datetime.endDate"
-          label="End Date"
-          placeholder="End Date"
+          label={t.endDate[lang]}
+          placeholder={t.endDate[lang]}
           containerClassName="flex-1"
           className="w-full"
           type="date"
@@ -120,16 +124,14 @@ export const DatetimeForm: React.FC<DatetimeFormProps> = ({
             isInvalidInput ? 'cursor-not-allowed' : ''
           }`}
         />
-        <Typography color="base">
-          Custom date and time (Different start and end time for each date)
-        </Typography>
+        <Typography color="base">{t.customDateAndTime[lang]}</Typography>
         {isInvalidInput ? (
           <Typography
             size="sm"
             style={{ whiteSpace: 'nowrap' }}
             className="pointer-events-none absolute -top-8 rounded-md bg-red-600 py-1 px-2 text-center text-white opacity-0 transition-all group-hover:opacity-100"
           >
-            Please provide valid start date and end date for your event above
+            {t.invalidDateTime[lang]}
           </Typography>
         ) : null}
       </label>
