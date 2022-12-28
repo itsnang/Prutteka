d060cbb81fb1e3f7e5060c66b39adb1c91f5a5e0
Chhayyy <huotchhayyy@gmail.com>
2022-12-28 10:10:26 +0700

    fix ui bug Button.tsx InterestedEventPage.tsx

--- packages/ui/src/Button.tsx
+++ packages/ui/src/Button.tsx
@@ -7,6 +7,7 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   className?: string;
   hasShadow?: boolean;
   fullWidth?: boolean;
+  roundedFull?: boolean;
   icon?: (props: React.ComponentProps<'svg'>) => JSX.Element;
 }
 
@@ -34,17 +35,18 @@ export const Button: React.FC<RequireChildrenOrIcon> = ({
   icon,
   href,
   fullWidth,
+  roundedFull,
   ...props
 }) => {
   const Icon = icon;
   const iconClassName = `sm:h-6 h-5 w-5 sm:w-6 ${
     children ? 'mr-[0.625rem]' : ''
   }`;
-  const componentClassname = `sm:h-14 h-12 inline-flex min-w-[3rem] sm:min-w-[3.5rem] items-center justify-center rounded-xl lg:rounded-2xl font-medium ${
+  const componentClassname = `sm:h-14 h-12 inline-flex min-w-[3rem] sm:min-w-[3.5rem] items-center justify-center font-medium ${
     variantClassname[variant]
   } ${fullWidth ? 'w-full' : ''} ${className ? className : ''} ${
     hasShadow ? 'shadow-inner' : ''
-  }`;
+  } ${roundedFull ? 'rounded-full' : 'rounded-xl lg:rounded-2xl'}`;
 
   if (as === 'link') {
     return (
