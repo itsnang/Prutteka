bae735027f1024defca9f90a255c6776f201291e
Hourmeng Khy <itshourmeng@gmail.com>
2023-02-09 08:53:36 +0700

    add event model & types

---
+++ apps/server/src/models/event.types.ts
@@ -0,0 +1,36 @@
+interface Time {
+  start_time: Date;
+  end_time: Date;
+}
+
+interface ScheduleItem {
+  start_time: Date;
+  end_time: Date;
+  activity: Translation;
+}
+
+export interface Translation {
+  en: string;
+  kh: string;
+}
+
+export interface DateTime {
+  start_date: Date;
+  end_date: Date;
+  times: Time[];
+}
+
+export interface Location {
+  name: Translation;
+  link: string;
+}
+
+export interface JoinMethod {
+  method: Translation;
+  link: string;
+}
+
+export interface Schedule {
+  date: Date;
+  schedules: ScheduleItem[];
+}
