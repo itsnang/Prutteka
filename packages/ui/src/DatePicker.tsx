489f551e46e7c993b33cacfe623bddf0d390e79d
Chhayyy <huotchhayyy@gmail.com>
2023-02-15 09:46:58 +0700

    fix DatePicker.tsx

--- packages/ui/src/DatePicker.tsx
+++ packages/ui/src/DatePicker.tsx
@@ -1,13 +1,14 @@
 import React, { forwardRef } from 'react';
+import { useState } from 'react';
 
-interface DatePickerProps
-  extends Partial<React.HTMLAttributes<HTMLInputElement>> {
+interface DatePickerProps {
   name: string;
   type?: string;
   label?: string;
   className?: string;
   containerClassName?: string;
   value: string;
+  onChange?: (value: React.SetStateAction<string>) => void;
 }
 
 export const DatePicker = forwardRef<HTMLInputElement, DatePickerProps>(
@@ -18,21 +19,31 @@ export const DatePicker = forwardRef<HTMLInputElement, DatePickerProps>(
       type = 'date',
       className = '',
       containerClassName = '',
+      value = '',
+      onChange,
       ...props
     },
     ref
   ) => {
+    const [input, setInput] = useState(value);
+
     return (
       <div className={`space-y-2 ${containerClassName}`}>
         <label className="flex flex-col">
           {label}
           <input
+            onChange={(e) => {
+              const value = e.target.value;
+              setInput(e.target.value);
+              onChange && onChange(value);
+            }}
+            value={input}
             type={type}
             className={`h-13 focus:ring-primary rounded-xl border border-gray-200 px-4 text-gray-900 focus:outline-none focus:ring ${className} ${
               label ? 'mt-2' : ''
             } ${type === 'date' ? 'form-input focus:border-none' : ''} `}
-            {...props}
             ref={ref}
+            {...props}
           />
         </label>
       </div>
