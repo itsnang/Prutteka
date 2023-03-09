9f09a744a8cbefb3bac495595049247d1c906a73
Sean Sophearom <sean.sophearom77@gmail.com>
2023-03-09 22:20:15 +0700

    backend for posting and updating event

--- apps/server/src/routes/event.routes.ts
+++ apps/server/src/routes/event.routes.ts
@@ -1,17 +1,29 @@
 import express from 'express';
+import imageUpload from '../utils/ImageUpload';
 
 // controllers
 import {
   getAllEvents,
   getEvent,
   createEvent,
+  updateEvent,
+  createNestedEvent,
 } from '../controllers/event.controllers';
-import multer from '../utils/multer';
 
 const router = express.Router();
 
-router.route('/').get(getAllEvents).post(multer, createEvent);
+router
+  .route('/')
+  .get(getAllEvents)
+  .post(imageUpload.eventFormMulter, createEvent);
 
-router.route('/:eventId').get(getEvent);
+router
+  .route('/:eventId')
+  .get(getEvent)
+  .put(imageUpload.eventFormMulter, updateEvent);
+
+router
+  .route('/:eventId/nested')
+  .post(imageUpload.eventFormMulter, createNestedEvent);
 
 export default router;
