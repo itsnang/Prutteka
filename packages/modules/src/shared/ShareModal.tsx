bb68c2e9141dff3230eb4ac3d4bed012e998b438
Hourmeng Khy <itshourmeng@gmail.com>
2023-04-19 19:28:07 +0700

    fix more icon not visible on ios devices

--- packages/modules/src/shared/ShareModal.tsx
+++ packages/modules/src/shared/ShareModal.tsx
@@ -76,7 +76,20 @@ export const ShareModal: React.FC<ShareModalProps> = ({
             <TelegramIcon />
           </IconStyle>
           <IconStyle onClick={handleShare}>
-            <EllipsisHorizontalIcon />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="h-6 w-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+              />
+            </svg>
           </IconStyle>
         </div>
       </div>
